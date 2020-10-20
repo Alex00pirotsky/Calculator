bc0a742ff23140f7605ab74f9f9f800d0ff6bc40
Ivan Filimonov <ivanfilimonov@MacBook-Air-S.local>
2020-10-20 20:05:01 +0300

    fix space bug

--- src/main.c
+++ src/main.c
@@ -41,22 +41,50 @@ char    *conclusion(char *arg)
     (have_it(beg->data, '(')) ? beg->data = conclusion(beg->data) : 0;
     return beg->data;
 }
+char *add_space(char *ar)
+{
+    char *n_ar;
+    int j;
 
+    j = 0;
+    n_ar = malloc(sizeof(char) * ft_str_len(ar) * 3);
+
+    for (int i = 0; i < ft_str_len(ar); ++i)
+    {
+        if(ar[i] == '+' || ar[i] == '-' || ar[i] == '*' || ar[i] == '/')
+        {
+            if(i > 0 && ar[i - 1] != ' ')
+            {
+                n_ar[j] = ' ';
+                j++;
+            }
+            if(ar[i + 1] != ' ')
+            {
+                n_ar[j] = ar[i];
+                n_ar[j + 1] = ' ';
+                j += 2;
+                i++;
+            }
+        }
+        n_ar[j] = ar[i];
+        j++;
+    }
+    return n_ar;
+}
 int main(int argc, char **argv)
 {
-
     printf("%s\n", argv[1]);
-    if(argc != 1)
+    if(argc != 2)
     {
     	printf("YOU should write only one arg");
 	return 0;
     }
+    char *j = add_space(argv[1]);
+
+    j = conclusion(j);
 
 
-    char *j = conclusion(argv[1]);
-
-
-    printf("\n%s\n", j);
+    printf("%s\n", j);
 
     return 0;
 }
